af1f1ead8f10d73c98b462fa68c614e2fcbb0538
Flow <highflowey@gmail.com>
2024-03-01 23:13:39 +0330

    Removed one of the cover arts

--- src/assets/covers.ts
+++ src/assets/covers.ts
@@ -1,9 +1,7 @@
 import galaxy from "./covers/galaxy.png";
-import berserk01 from "./covers/berserk01.jpeg";
 import redmooncity from "./covers/redmooncity.jpg";
 
 export default {
   galaxy,
-  berserk01,
   redmooncity,
 };
