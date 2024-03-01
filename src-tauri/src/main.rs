f5f5b305e4f6b460035ba244ec8f7d2d5f0aac3d
Flow <highflowey@gmail.com>
2024-03-01 17:33:52 +0330

    Safer code added more if statements to make the backend rust code more safe

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,46 +2,57 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use audiotags::Tag;
-use mp3_duration;
 use serde::Serialize;
-use std::{fs::read_dir, path::PathBuf, time::Duration};
+use std::{ffi::OsString, fs::read_dir, path::PathBuf, time::Duration};
 use tauri::{AppHandle, Manager};
 
 #[derive(Debug, Serialize)]
 struct FileInfo {
     duration: f64,
     path: PathBuf,
-    name: String,
+    name: OsString,
     artist: String,
 }
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn read_directory(directory_url: &str) -> Vec<FileInfo> {
-    let mut entries: Vec<FileInfo> = Vec::new();
+    let directory = read_dir(directory_url);
 
-    for entry in read_dir(directory_url).unwrap().into_iter() {
-        let entry_result = entry.unwrap();
-        let tag_result = Tag::new().read_from_path(entry_result.path());
+    if directory.is_ok() {
+        let mut entries: Vec<FileInfo> = Vec::new();
 
-        if tag_result.is_ok() {
-            let tag = tag_result.unwrap();
-            entries.push(FileInfo {
-                duration: mp3_duration::from_path(entry_result.path())
-                    .unwrap_or_else(|_| Duration::from_secs(0))
-                    .as_secs_f64(),
-                path: entry_result.path(),
-                name: entry_result.file_name().into_string().unwrap(),
-                artist: if tag.artist().is_some() {
-                    tag.artist().unwrap().to_owned()
-                } else {
-                    String::from("Uknown Artist")
-                },
-            });
+        for entry in directory.unwrap().into_iter() {
+            if entry.is_err() {
+                continue;
+            }
+
+            let entry_result = entry.unwrap();
+            let tag_result = Tag::new().read_from_path(entry_result.path());
+
+            if tag_result.is_ok() {
+                let tag = tag_result.unwrap();
+                let artist = tag.artist();
+
+                entries.push(FileInfo {
+                    duration: mp3_duration::from_path(entry_result.path())
+                        .unwrap_or_else(|_| Duration::from_secs(0))
+                        .as_secs_f64(),
+                    path: entry_result.path(),
+                    name: entry_result.file_name(),
+                    artist: if artist.is_some() {
+                        artist.unwrap().to_owned()
+                    } else {
+                        String::from("Uknown Artist")
+                    },
+                });
+            }
         }
+
+        return entries;
     }
 
-    return entries;
+    return Vec::new();
 }
 
 #[tauri::command]
